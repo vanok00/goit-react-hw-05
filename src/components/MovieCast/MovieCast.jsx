52b50635aaea40f31c24bbb18d961679ff4d2538
Vanok <vanok008@ukr.net>
2024-09-26 21:09:46 +0300

    actors

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchCastsById } from "../../services/api";
+import s from "./MovieCast.module.css";
+
 const MovieCast = () => {
-  return <div></div>;
+  const { moviesId } = useParams();
+  const [casts, setCasts] = useState([]);
+  useEffect(() => {
+    const getCasts = async () => {
+      if (!moviesId) return;
+      const casts = await fetchCastsById(moviesId);
+      setCasts(casts);
+    };
+    getCasts();
+  }, [moviesId]);
+  return (
+    <div>
+      <ul className={s.actorList}>
+        {casts.map((cast) => (
+          <li key={cast.id}>
+            <div className={s.actor}>
+              <img
+                width={100}
+                height={150}
+                src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
+                alt={cast.title}
+              />
+              <p>{cast.name}</p>
+              <p>Character:{cast.character}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MovieCast;
+
+// "profile_path": "/DDeITcCpnBd0CkAIRPhggy9bt5.jpg"
--- src/services/api.js
+++ src/services/api.js
@@ -27,23 +27,37 @@ export const fetchMovieById = async (moviesId) => {
   }
 };
 
-export const fetchGenres = async (moviesId) => {
+// export const fetchGenres = async (moviesId) => {
+//   const YOUR_ACCESS_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWM4ODQ3MTIyNjQwMWFjNjg4NGE5OGQ4NzA0YjcxZCIsIm5iZiI6MTcyNzI0MzUyNS4zMTE5NzQsInN1YiI6IjY2ZjNhM2M4ZjViNDk3ODY0MzIyZTc5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tOtm59wrEpA1AFBRsaAl7cHNRePqrN46NYcLozi5JO0";
+
+//   try {
+//     const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en/${moviesId}`, {
+
+//       headers: {
+//         Authorization: `Bearer ${YOUR_ACCESS_KEY}`,
+//       },
+//     });
+// const genres = response.data.genres;
+// return genres;
+//   } catch (error) {
+//     console.error("Error fetching movie data:", error);
+//     return[];
+//   }
+// };
+// fetchGenres();
+
+
+
+export const fetchCastsById = async (moviesId) => {
   const YOUR_ACCESS_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MWM4ODQ3MTIyNjQwMWFjNjg4NGE5OGQ4NzA0YjcxZCIsIm5iZiI6MTcyNzI0MzUyNS4zMTE5NzQsInN1YiI6IjY2ZjNhM2M4ZjViNDk3ODY0MzIyZTc5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tOtm59wrEpA1AFBRsaAl7cHNRePqrN46NYcLozi5JO0";
 
-  try {
-    const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?language=en/${moviesId}`, {
-
-      headers: {
-        Authorization: `Bearer ${YOUR_ACCESS_KEY}`,
-      },
-    });
-const genres = response.data.genres;
-return genres;
-  } catch (error) {
-    console.error("Error fetching movie data:", error);
-    return[];
-  }
+  const response = await axios.get(`https://api.themoviedb.org/3/movie/${moviesId}/credits`, {
+    headers: {
+      Authorization: `Bearer ${YOUR_ACCESS_KEY}`,
+    },
+  });
+  return response.data.cast; 
 };
-fetchGenres();
+
 
 
