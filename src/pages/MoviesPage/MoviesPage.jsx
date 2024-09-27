e73d9d120cd84520664adffff0e54e805cdf29a7
Vanok <vanok008@ukr.net>
2024-09-27 10:43:26 +0300

    searchbar

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,44 @@
-const MoviesPage = () => {
-  return <div></div>;
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { Field, Form, Formik } from "formik";
+import toast, { Toaster } from "react-hot-toast";
+import s from "./MoviesPage.module.css";
+
+const MoviesPage = ({ setQuery }) => {
+  const initialValues = {
+    query: "",
+  };
+  const handleSubmit = (values) => {
+    if (!values.query) {
+      toast.error("Sorry, your input field can`t be empty!", {
+        style: {
+          color: "red",
+        },
+        duration: 3000,
+        position: "top-right",
+      });
+      return;
+    }
+    setQuery(values.query);
+  };
+
+  return (
+    <div>
+      <Toaster />
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={s.searchBar}>
+          <button type="submit" className={s.searchBtn}>
+            <FaMagnifyingGlass />
+          </button>
+          <Field
+            className={s.searchInput}
+            name="query"
+            type="text"
+            placeholder="Search images and photos"
+          />
+        </Form>
+      </Formik>
+    </div>
+  );
 };
+
 export default MoviesPage;
