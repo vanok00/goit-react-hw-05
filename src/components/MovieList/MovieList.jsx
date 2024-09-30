005263ca9039c68a808578061d197e46b22ad92d
Vanok <vanok008@ukr.net>
2024-09-30 19:59:37 +0300

    movies

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,26 +1,58 @@
+// import s from "./MovieList.module.css";
+// import { Link, useLocation } from "react-router-dom";
+
+// const MovieList = ({ movies }) => {
+//   const location = useLocation();
+
+//   if (!movies || movies.length === 0) {
+//     return <p>IDI NAHUY</p>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Trending today</h1>
+//       <ul className={s.moviesList}>
+//         {movies.map((movie) => (
+//           <li key={movie.id}>
+//             <Link to={`/movie/${movie.id}`} state={location}>
+//               <p>{movie.title}</p>
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default MovieList;
+
+import { useState } from "react";
+import fetchMovies from "../../services/api";
 import s from "./MovieList.module.css";
-import { Link, useLocation } from "react-router-dom";
-
-const MovieList = ({ movies }) => {
-  const location = useLocation();
-
-  if (!movies || movies.length === 0) {
-    return <p>IDI NAHUY</p>;
-  }
-
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+const MovieList = ({ query }) => {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    const getTrendingMovies = async () => {
+      const movies = await fetchMovies();
+      setResults(movies || []);
+    };
+    getTrendingMovies();
+  }, [query]);
   return (
-    <div>
-      <h1>Trending today</h1>
+    <>
+      <h1>{query ? `Search results for: ${query}` : "Trending today"}</h1>
       <ul className={s.moviesList}>
-        {movies.map((movie) => (
+        {results.map((movie) => (
           <li key={movie.id}>
-            <Link to={`/movie/${movie.id}`} state={location}>
+            <Link to={`/movie/${movie.id}`}>
               <p>{movie.title}</p>
             </Link>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
