4d1fac267b9cc50882b613ef7421909e2b6efaff
Vanok <vanok008@ukr.net>
2024-10-01 10:48:45 +0300

    State

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,3 +1,81 @@
+// import { FaMagnifyingGlass } from "react-icons/fa6";
+// import { Field, Form, Formik } from "formik";
+// import { Toaster } from "react-hot-toast";
+// import s from "./MoviesPage.module.css";
+// import { useState } from "react";
+// import MovieList from "../../components/MovieList/MovieList";
+// import { useMemo } from "react";
+// import { fetchMovieByQuery } from "../../services/api";
+// import { useSearchParams } from "react-router-dom";
+// import { useEffect } from "react";
+
+// const MoviesPage = () => {
+//   const [movies, setMovies] = useState([]);
+
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const query = searchParams.get("query") ?? "";
+//   useEffect(() => {
+//     const getData = async () => {
+//       if (query) {
+//         const data = await fetchMovieByQuery(query);
+//         setMovies(data);
+//       }
+//     };
+//     getData();
+//   }, [query]);
+
+//   const handleChangeQuery = (newQuery) => {
+//     if (!newQuery) {
+//       setSearchParams({ query: newQuery });
+//     } else {
+//       setSearchParams({});
+//     }
+//   };
+
+//   const initialValues = {
+//     query: "",
+//   };
+//   console.log(query);
+
+//   const handleSubmit = (values) => {
+//     handleChangeQuery(values.query);
+//   };
+
+//   const filteredMovies = useMemo(
+//     () =>
+//       movies.filter((movie) =>
+//         movie.title.toLowerCase().includes(query.toLowerCase())
+//       ),
+//     [query, movies]
+//   );
+
+//   return (
+//     <>
+//       <div>
+//         <Toaster />
+//         <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+//           <Form className={s.searchBar}>
+//             <button type="submit" className={s.searchBtn}>
+//               <FaMagnifyingGlass />
+//             </button>
+//             <Field
+//               className={s.searchInput}
+//               name="query"
+//               type="text"
+//               placeholder="Search movies..."
+//             />
+//           </Form>
+//         </Formik>
+//       </div>
+//       <div>
+//         <MovieList movies={filteredMovies} />
+//       </div>
+//     </>
+//   );
+// };
+
+// export default MoviesPage;
+
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { Field, Form, Formik } from "formik";
 import { Toaster } from "react-hot-toast";
@@ -10,50 +88,28 @@ import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 
 const MoviesPage = () => {
-  const [movies, setMovies] = useState([]);
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get("query") ?? "";
-  useEffect(() => {
-    const getData = async () => {
-      if (query) {
-        const data = await fetchMovieByQuery(query);
-        setMovies(data);
-      }
-    };
-    getData();
-  }, [query]);
-
-  const handleChangeQuery = (newQuery) => {
-    if (!newQuery) {
-      setSearchParams({ query: newQuery });
-    } else {
-      setSearchParams({});
-    }
-  };
-
+  const [query, setQuery] = useState();
   const initialValues = {
     query: "",
   };
-  console.log(query);
-
   const handleSubmit = (values) => {
+    console.log(values);
     handleChangeQuery(values.query);
   };
 
-  const filteredMovies = useMemo(
-    () =>
-      movies.filter((movie) =>
-        movie.title.toLowerCase().includes(query.toLowerCase())
-      ),
-    [query, movies]
-  );
+  const handleChangeQuery = (newQuery) => {
+    setQuery(newQuery);
+  };
 
   return (
     <>
       <div>
         <Toaster />
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          handleChangeQuery={handleChangeQuery}
+        >
           <Form className={s.searchBar}>
             <button type="submit" className={s.searchBtn}>
               <FaMagnifyingGlass />
@@ -67,9 +123,7 @@ const MoviesPage = () => {
           </Form>
         </Formik>
       </div>
-      <div>
-        <MovieList movies={filteredMovies} />
-      </div>
+      <div>{/* <MovieList movies={filteredMovies} /> */}</div>
     </>
   );
 };
